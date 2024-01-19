6b06656d6b11f0f0859431a2f6ad3981817fa9b6
tensoid <iubaqua0@gmail.com>
2024-01-19 21:12:57 +0100

    implemented removing and adding columns to table

--- src/es-utils/mcclusky.ts
+++ src/es-utils/mcclusky.ts
@@ -1,23 +1,27 @@
-type Bits3 = Array<'-' | '0' | '1'>;
+type Bits3 = Array<"-" | "0" | "1">;
 
 interface MintermTableRow {
-  minterms: Set<number>,
-  bits: Bits3,
-  combined: boolean
+  minterms: Set<number>;
+  bits: Bits3;
+  combined: boolean;
 }
 
 type MintermTable = Array<MintermTableRow>;
 
 /**
  * Minimizes a logical function using the Quine-McCluskey Algorithm.
- * @param inputNames 
- * @param minterms 
+ * @param inputNames
+ * @param minterms
  * @returns The minimized logical expression
  */
-export function computeMcClusky(inputNames: Array<string>, minterms: Set<number>, dontCares: Set<number>): string {
-
-  if(minterms.size == 0) return "0";
-  if(minterms.size + dontCares.size == Math.pow(2, inputNames.length)) return "1";
+export function computeMcClusky(
+  inputNames: Array<string>,
+  minterms: Set<number>,
+  dontCares: Set<number>
+): string {
+  if (minterms.size == 0) return "0";
+  if (minterms.size + dontCares.size == Math.pow(2, inputNames.length))
+    return "1";
 
   // Step 1: finding prime implicants
 
@@ -25,33 +29,37 @@ export function computeMcClusky(inputNames: Array<string>, minterms: Set<number>
 
   // create first table
   let initialMintermTable: MintermTable = [];
-  [...minterms, ...dontCares].forEach(minterm => {
+  [...minterms, ...dontCares].forEach((minterm) => {
     initialMintermTable.push({
       minterms: new Set([minterm]),
-      bits: minterm.toString(2).padStart(inputNames.length, '0').split('') as Bits3,
-      combined: false
+      bits: minterm
+        .toString(2)
+        .padStart(inputNames.length, "0")
+        .split("") as Bits3,
+      combined: false,
     });
   });
 
   mintermTables.push(initialMintermTable);
 
-  for(let iteration = 1;; iteration++) {
-
+  for (let iteration = 1; ; iteration++) {
     // create new table
     mintermTables.push([]);
 
     // get each combination
     mintermTables[iteration - 1].forEach((row, i) => {
-      mintermTables[iteration - 1].slice(i + 1).forEach(otherRow => {
+      mintermTables[iteration - 1].slice(i + 1).forEach((otherRow) => {
         // check if bit difference is 1
         let bitDifferences = getBitDifferences(row.bits, otherRow.bits);
-        if(bitDifferences.length > 1) return;
+        if (bitDifferences.length > 1) return;
 
         // add row to next table
         mintermTables[iteration].push({
           minterms: new Set([...row.minterms, ...otherRow.minterms]),
-          bits: row.bits.map((bit, bitIndex) => bitIndex == bitDifferences[0] ? '-' : bit),
-          combined: false
+          bits: row.bits.map((bit, bitIndex) =>
+            bitIndex == bitDifferences[0] ? "-" : bit
+          ),
+          combined: false,
         });
 
         // mark both rows as combined
@@ -59,9 +67,9 @@ export function computeMcClusky(inputNames: Array<string>, minterms: Set<number>
         otherRow.combined = true;
       });
     });
-          
+
     // check if finished combining
-    if(mintermTables[iteration].length == 0) {
+    if (mintermTables[iteration].length == 0) {
       mintermTables.pop();
       break;
     }
@@ -69,57 +77,85 @@ export function computeMcClusky(inputNames: Array<string>, minterms: Set<number>
 
   // Step 2: prime implicant chart
 
-  let primeImplicants = mintermTables.flat().filter(row => !row.combined);
-  let essentialPrimeImplicants = primeImplicants.filter(pi => {
+  let primeImplicants = mintermTables.flat().filter((row) => !row.combined);
+  let essentialPrimeImplicants = primeImplicants.filter((pi) => {
     // check if a minterm exists that is in no other prime implicant
-    return [...pi.minterms].find(mt => {
-      if([...dontCares].includes(mt)) return false;
-      return primeImplicants.flatMap(pi2 => [...pi2.minterms]).filter(mt2 => mt2 == mt).length == 1;
-    }) != undefined;
+    return (
+      [...pi.minterms].find((mt) => {
+        if ([...dontCares].includes(mt)) return false;
+        return (
+          primeImplicants
+            .flatMap((pi2) => [...pi2.minterms])
+            .filter((mt2) => mt2 == mt).length == 1
+        );
+      }) != undefined
+    );
   });
 
-  let notCoveredMinterms: Set<number> = new Set([...minterms].filter(mt => {
-    return !essentialPrimeImplicants.flatMap(pi => [...pi.minterms]).includes(mt);
-  }));
+  let notCoveredMinterms: Set<number> = new Set(
+    [...minterms].filter((mt) => {
+      return !essentialPrimeImplicants
+        .flatMap((pi) => [...pi.minterms])
+        .includes(mt);
+    })
+  );
 
-  if(notCoveredMinterms.size > 0) {
-    let primeImplicantsPowerSet = getPowerSet(new Set(primeImplicants)).map(set => [...set]);
-    let coveringPrimeImplicantsPowerSet = primeImplicantsPowerSet.filter(pis => {
-      let coveredMinterms = pis.flatMap(pi => [...pi.minterms]);
-      return [...notCoveredMinterms].every(minterm => coveredMinterms.includes(minterm));
-    });
-    
+  if (notCoveredMinterms.size > 0) {
+    let primeImplicantsPowerSet = getPowerSet(new Set(primeImplicants)).map(
+      (set) => [...set]
+    );
+    let coveringPrimeImplicantsPowerSet = primeImplicantsPowerSet.filter(
+      (pis) => {
+        let coveredMinterms = pis.flatMap((pi) => [...pi.minterms]);
+        return [...notCoveredMinterms].every((minterm) =>
+          coveredMinterms.includes(minterm)
+        );
+      }
+    );
+
     coveringPrimeImplicantsPowerSet.sort((a, b) => b.length - a.length);
-    coveringPrimeImplicantsPowerSet.sort((a, b) => a.flatMap(m => m.minterms).length - b.flatMap(m => m.minterms).length);
+    coveringPrimeImplicantsPowerSet.sort(
+      (a, b) =>
+        a.flatMap((m) => m.minterms).length -
+        b.flatMap((m) => m.minterms).length
+    );
 
     essentialPrimeImplicants.push(...coveringPrimeImplicantsPowerSet[0]);
   }
-  
+
   // convert essential prime implicants to expression string
-  return essentialPrimeImplicants.map(pi => {
-    let conjunction = pi.bits.map((bit, bitIndex) => {
-      if(bit == '-') return "";
-      else if(bit == '0') return `¬${inputNames[bitIndex]}`;
-      else return `${inputNames[bitIndex]}`;
-    }).filter(c => c != "").join(" ∧ ");
-    
-    return "(" + conjunction + ")";
-  }).join(" ∨ ");
+  return essentialPrimeImplicants
+    .map((pi) => {
+      let conjunction = pi.bits
+        .map((bit, bitIndex) => {
+          if (bit == "-") return "";
+          else if (bit == "0") return `¬${inputNames[bitIndex]}`;
+          else return `${inputNames[bitIndex]}`;
+        })
+        .filter((c) => c != "")
+        .join(" ∧ ");
+
+      return "(" + conjunction + ")";
+    })
+    .join(" ∨ ");
 }
 
 function getBitDifferences(input1: Bits3, input2: Bits3): Array<number> {
-
   let differences: Array<number> = [];
 
   input1.forEach((bit, index) => {
-    if(bit != input2[index]) differences.push(index);
+    if (bit != input2[index]) differences.push(index);
   });
 
   return differences;
 }
 
 function getPowerSet<T>(set: Set<T>): Array<Set<T>> {
-  return [...set].reduce(
-      (subsets, value) => subsets.concat(subsets.map(subset => [value,...subset])),[[]] as Array<Array<T>>)
-      .map(arr => new Set(arr));
+  return [...set]
+    .reduce(
+      (subsets, value) =>
+        subsets.concat(subsets.map((subset) => [value, ...subset])),
+      [[]] as Array<Array<T>>
+    )
+    .map((arr) => new Set(arr));
 }
--- src/views/McCluskeyView.vue
+++ src/views/McCluskeyView.vue
@@ -1,62 +1,81 @@
 <script setup lang="ts">
-
-import { ref, computed } from 'vue'
-import { computeMcClusky } from '../es-utils/mcclusky'
-
-
-
+import { ref, computed } from "vue";
+import { computeMcClusky } from "../es-utils/mcclusky";
 
 // Input Table
 const inputs = ref(["S0", "S1", "S2"]);
 
 const truthTableRows = computed(() => Math.pow(2, inputs.value.length));
 
-function addInput(){
-  inputs.value.push("");
+function addInput() {
+  inputs.value.push("?");
+
+  outputs.value.forEach((func) => {
+    func.outputs.push(...Array<OutputBit>(func.outputs.length).fill("0"));
+  });
 }
 
+function removeInput(index: number) {
+  if(inputs.value.length == 1) return;
+  inputs.value.splice(index, 1);
+
+  outputs.value.forEach((func) => {
+    func.outputs.splice(func.outputs.length / 2);
+  });
+}
 
 // Output Table
-type OutputBit = '0' | '1' | 'X';
+type OutputBit = "0" | "1" | "X";
 
 interface LogicFunction {
-  name: string,
-  outputs: Array<OutputBit>
+  name: string;
+  outputs: Array<OutputBit>;
 }
 
 const outputs = ref([] as Array<LogicFunction>);
 
 outputs.value.push({
   name: "F",
-  outputs: Array<OutputBit>(truthTableRows.value).fill("0")
+  outputs: Array<OutputBit>(truthTableRows.value).fill("0"),
 });
 
-function addLogicFunction(){
+function addLogicFunction() {
   outputs.value.push({
-    name: "",
-    outputs: Array<OutputBit>(truthTableRows.value).fill("0")
+    name: "?",
+    outputs: Array<OutputBit>(truthTableRows.value).fill("0"),
   });
-
-  // TODO: update other functions
 }
 
-function toggleBit(colIndex: number, rowIndex: number){
+function removeLogicFunction(index: number) {
+  if(outputs.value.length == 1) return;
+  outputs.value.splice(index, 1);
+}
+
+function toggleBit(colIndex: number, rowIndex: number) {
   let value = outputs.value[colIndex].outputs[rowIndex - 1];
-  if(value == '0') outputs.value[colIndex].outputs[rowIndex - 1] = '1';
-  else if(value == '1') outputs.value[colIndex].outputs[rowIndex - 1] = 'X';
-  else outputs.value[colIndex].outputs[rowIndex - 1] = '0';
+  if (value == "0") outputs.value[colIndex].outputs[rowIndex - 1] = "1";
+  else if (value == "1") outputs.value[colIndex].outputs[rowIndex - 1] = "X";
+  else outputs.value[colIndex].outputs[rowIndex - 1] = "0";
 }
 
 // Results
 const minimizedFunctions = computed(() => {
-  return outputs.value.map(func => {
-    const minterms = new Set(func.outputs.reduce((terms, bit, index) => bit == '1' ? [...terms, index] : terms, [] as Array<number>));
-    const dontCares = new Set(func.outputs.reduce((terms, bit, index) => bit == 'X' ? [...terms, index]: terms, [] as Array<number>));
-    console.log(dontCares);
+  return outputs.value.map((func) => {
+    const minterms = new Set(
+      func.outputs.reduce(
+        (terms, bit, index) => (bit == "1" ? [...terms, index] : terms),
+        [] as Array<number>
+      )
+    );
+    const dontCares = new Set(
+      func.outputs.reduce(
+        (terms, bit, index) => (bit == "X" ? [...terms, index] : terms),
+        [] as Array<number>
+      )
+    );
     return computeMcClusky(inputs.value, minterms, dontCares);
-  })
+  });
 });
-
 </script>
 
 <template>
@@ -64,55 +83,117 @@ const minimizedFunctions = computed(() => {
     <table class="inputs-table">
       <tr>
         <th v-for="(_, index) in inputs">
-          <input v-model="inputs[index]">
+          <div class="table-head">
+            <button @click="removeInput(index)" class="remove-button">
+              <svg width="20" height="20">
+                <image href="@/assets/icons/cross.svg" />
+              </svg>
+            </button>
+            <input v-model="inputs[index]" />
+          </div>
         </th>
         <th>
           <button class="add-button" @click="addInput">+</button>
         </th>
       </tr>
       <tr v-for="rowIndex in truthTableRows">
-        <td class="bit" v-for="colIndex in inputs.length" >{{ ((rowIndex - 1) >> (inputs.length - colIndex)) & 1}}</td>
+        <td class="bit" v-for="colIndex in inputs.length">
+          {{ ((rowIndex - 1) >> (inputs.length - colIndex)) & 1 }}
+        </td>
       </tr>
     </table>
 
     <table class="outputs-table">
       <tr>
-        <th v-for="func in outputs">{{ func.name }}</th>
+        <th v-for="(_, index) in outputs">
+          <div class="table-head">
+            <button @click="removeLogicFunction(index)" class="remove-button">
+              <svg width="20" height="20">
+                <image href="@/assets/icons/cross.svg" />
+              </svg>
+            </button>
+            <input v-model="outputs[index].name" />
+          </div>
+        </th>
         <th>
           <button class="add-button" @click="addLogicFunction">+</button>
         </th>
       </tr>
       <tr v-for="rowIndex in truthTableRows">
-        <td class="bit" @click="toggleBit(colIndex, rowIndex)" v-for="(func, colIndex) in outputs" >{{ func.outputs[rowIndex - 1] }}</td>
+        <td
+          class="bit"
+          @click="toggleBit(colIndex, rowIndex)"
+          v-for="(func, colIndex) in outputs"
+        >
+          {{ func.outputs[rowIndex - 1] }}
+        </td>
       </tr>
     </table>
   </div>
-  
-  <p v-for="(func, index) in minimizedFunctions" >{{ outputs[index].name + ": " + func }}</p>
+
+  <p v-for="(func, index) in minimizedFunctions">
+    {{ outputs[index].name + ": " + func }}
+  </p>
 </template>
 
 <style scoped lang="scss">
-
 .tables {
-
   display: flex;
   flex-direction: row;
 
-  .inputs-table, .outputs-table {
+  .inputs-table,
+  .outputs-table {
     border: 1px solid var(--color-table-border);
     border-collapse: collapse;
-
-    input {
-      width: 5ch;
-      background-color: transparent;
-      color: white;
-      border: none;
-      text-align: center;
-    }
 
     th {
       width: 5ch;
       border: 1px solid var(--color-table-border);
+
+      .add-button {
+        background-color: transparent;
+        color: white;
+        border: none;
+        cursor: pointer;
+        transition: 0.3s;
+
+        &:hover {
+          background-color: rgba(255, 255, 255, 0.164);
+        }
+      }
+
+      .table-head {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+        input,
+        span {
+          width: 5ch;
+          background-color: transparent;
+          color: white;
+          border: none;
+          text-align: center;
+          height: 1.5rem;
+          margin: 0;
+          padding: 0;
+        }
+
+        .remove-button {
+          background-color: transparent;
+          color: white;
+          border: none;
+          padding: 1px;
+          cursor: pointer;
+
+          transition: 0.3s;
+
+          &:hover {
+            background-color: rgba(255, 255, 255, 0.164);
+          }
+        }
+      }
     }
 
     tr:nth-child(4n + 1) {
@@ -128,13 +209,6 @@ const minimizedFunctions = computed(() => {
       text-align: center;
       user-select: none;
     }
-
-    .add-button {
-      background-color: transparent;
-      color: white;
-      border: none;
-    }
   }
 }
-
 </style>
